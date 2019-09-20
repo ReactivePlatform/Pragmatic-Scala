9d200ab4eb4d755aeaa61760a08d85fe0fbdcd0e
Darcy Shen <sadhen@zoho.com>
2019-09-20 10:24:35 +0800

    Upgrade to sbt 1.3.0

--- src/test/java/chapter14/UseEquipment.java
+++ src/test/java/chapter14/UseEquipment.java
@@ -1,15 +1,17 @@
 /*
  * Copyright 2018 pragmatic-scala.reactiveplatform.xyz
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package chapter14;
@@ -22,7 +24,7 @@ public class UseEquipment {
 
   public static void main(String[] args) {
     Equipment equipment = new Equipment();
-    
+
     equipment.simulate(4, value -> printAndReturn(value));
   }
 }
--- src/test/java/chapter5/OKJava.java
+++ src/test/java/chapter5/OKJava.java
@@ -1,15 +1,17 @@
 /*
  * Copyright 2018 pragmatic-scala.reactiveplatform.xyz
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package chapter5;
@@ -19,13 +21,13 @@ import java.util.*;
 public class OKJava {
   public static void main(String[] args) {
     // #snip
-    //Java code
+    // Java code
     ArrayList<Integer> list = new ArrayList<Integer>();
     ArrayList<Object> list2 = list; // Compilation error
     // #snip
 
-    //#snip_1
+    // #snip_1
     ArrayList list3 = list;
-    //#snip_1
+    // #snip_1
   }
 }
